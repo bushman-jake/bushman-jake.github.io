3d398d29930f07ba7603a0f017fcf8eb5384f37a
SimpleBinary <simplebinary@yahoo.com>
2024-07-29 02:38:58 +0800

    Add Content

---
+++ src/routes/+page.svelte
@@ -0,0 +1,97 @@
+<div style="background-color: {bgColor}" class="content">
+    <div class="container">
+        <form on:submit|preventDefault={onSubmit} id="entry-form">
+            <h1>Welcome, Bushman!</h1>
+            <hr />
+            <fieldset>
+                <legend>Enter Code</legend>
+                <input bind:value={entryCode} type="text" name="code" id="entry-form-code" />
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</div>
+
+<script>
+    import { goto } from '$app/navigation';
+
+    const CODE = "let's go"
+
+    let entryCode = "";
+    let bgColor = "#8cd19b";
+
+    function onSubmit() {
+        if (entryCode.toLowerCase() === CODE) {
+            goto("/success");
+        } else {
+            bgColor = "#d35555";
+        }
+    }
+</script>
+
+<style>
+    .content {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: url("./pattern.png");
+        background-blend-mode: multiply;
+        background-size: clamp(100px, 100%, 600px);
+    }
+
+    .container {
+        width: clamp(100px, 80%, 800px);
+        height: clamp(100px, 90%, 600px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        border-radius: 20px;
+        box-shadow: 0px 20px 40px rgba(1, 34, 15, 0.568);
+        padding: 20px;
+    }
+
+    h1 {
+        font-family: "Red Hat Display", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 900;
+        font-style: normal;
+    }
+
+    hr {
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    fieldset, button, input {
+        width: 100%;
+        padding: 10px;
+    }
+
+    fieldset {
+        padding: 0;   
+    }
+
+    legend {
+        padding-inline: 5px;
+        margin-inline: 10px;
+    }
+
+    button {
+        margin: 10px 0;
+        border: 1px solid rgb(49, 49, 49);
+        outline: none;
+        cursor: pointer;
+    }
+
+    button:active {
+        background-color: #d2e9df;
+    }
+
+    input {
+        border: none;
+        outline: none;
+    }
+</style>
---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,53 @@
+<div class="content">
+    <div class="container">
+        <div class="card">
+            <h1>Success!</h1>
+            <hr />
+            <p>May luck be with you on your onward journey. がんばって(笑)!</p>
+            <br />
+            <p>As a token of our gratitide, accept this limerick:</p>
+            <br />
+            <i><p>The journey shall lead you from here,</p>
+            <p>To an object you hold so dear.</p>
+            <p>It's an auto. No clutch!</p>
+            <p>It's Korean. Not Dutch!</p>
+            <p>And has a wheel with which you can steer.</p></i>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container {
+        width: clamp(100px, 80%, 800px);
+        height: clamp(100px, 90%, 600px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        border-radius: 20px;
+        box-shadow: 0px 20px 40px rgba(1, 34, 15, 0.568);
+        padding: 20px;
+    }
+
+    h1 {
+        font-family: "Red Hat Display", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 900;
+        font-style: normal;
+    }
+
+    hr {
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+
+    .content {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: url("./sakura.png");
+        background-size: clamp(200px, 300%, 1200px);
+    }
+</style>
